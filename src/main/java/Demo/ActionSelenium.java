c2c479e59063d353931f3cec2e14c8d64b615862
daixiongkun <514590399@qq.com>
2018-10-26 01:18:40 +0800

    操作输入框与单选框

--- src/main/java/Demo/ActionSelenium.java
+++ src/main/java/Demo/ActionSelenium.java
@@ -2,34 +2,68 @@ package Demo;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.util.List;
+
+
 public class ActionSelenium {
-    public WebDriver driver;
     /*初始化浏览器驱动*/
+    public WebDriver driver;
     public void InitDriver(){
         System.setProperty("webdriver.chrome.driver","D:\\WorkSpace\\UITestStudy\\src\\main\\resources\\chromedriver.exe");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("headless");   //设置浏览器静默模式，后台运行浏览器
-        driver = new ChromeDriver(options);
+        //ChromeOptions options = new ChromeOptions();
+        //options.addArguments("headless");   //设置浏览器静默模式，后台运行浏览器
+        //driver = new ChromeDriver(options);
+        driver = new ChromeDriver();
         driver.get("http://www.imooc.com/user/newlogin/from_url");
         driver.manage().window().maximize();
     }
 
+    /*操作input输入框*/
     public void inputBox(){
         driver.findElement(By.name("email")).sendKeys("17576075478");
+        driver.findElement(By.name("password")).sendKeys("daixiongkun123");
+        driver.findElement(By.className("moco-btn")).click();
         try {
-            Thread.sleep(1000);
+            Thread.sleep(4000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        driver.findElement(By.name("email")).clear();
+    }
+
+    /*操作单选框*/
+    public void radioBox(){
+        driver.get("https://www.imooc.com/user/setprofile");
+        driver.findElement(By.className("pull-right")).click();
+        List<WebElement> elements = driver.findElements(By.xpath("//*[@id=\"profile\"]/div[4]/div/label//input"));//定位到label下的input元素
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        for (WebElement radio:elements
+             ) {
+            boolean flag = radio.isSelected();
+            if (flag == false){
+                radio.click();
+                break;
+            }else {
+                System.out.println("选中了");
+            }
+        }
     }
 
     public static void main(String[] args) {
         ActionSelenium as = new ActionSelenium();
         as.InitDriver();
         as.inputBox();
+        as.radioBox();
     }
 }
+
+
+
+
